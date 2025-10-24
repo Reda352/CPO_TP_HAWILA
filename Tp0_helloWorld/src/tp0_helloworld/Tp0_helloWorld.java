2d46a08c15c3da1f4469da9411d8c6658657405e
Reda352 <reda.hawila@epfedu.fr>
2025-10-24 11:46:35 +0200

    Tp1/1

---
+++ Tp0_helloWorld/src/tp0_helloworld/Tp0_helloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+
+import java.util.Scanner;
+
+/**Hawila Reda
+ *20/10/2025
+ * @author ADMIN
+ */
+public class Tp0_helloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {System.out.println( " Bonjour " ); 
+    System.out.println("au revoir");
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Quel est votre prénom ?");
+    sc.nextLine();  
+     
+// TODO code application logic here/
+    }
+    
+}
