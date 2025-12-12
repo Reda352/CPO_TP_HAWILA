572a4bee7288b07bac1fa3503d9c83234fff32e4
Reda352 <reda.hawila@epfedu.fr>
2025-12-12 15:34:09 +0100

    jeux ameliorer.

--- Cadena/src/CadenasJeu.java
+++ Cadena/src/CadenasJeu.java
@@ -9,41 +9,37 @@
  */
 import java.util.Random;
 
+import java.util.Random;
+
 public class CadenasJeu {
+
     private static final int LONGUEUR_CODE = 4;
     private static final int MAX_TENTATIVES = 5;
-    private static final int MIN_CHIFFRE = 0;
-    private static final int MAX_CHIFFRE = 9;
-   
+    private static final Random RAND = new Random();
+
     private int[] codeSecret;
     private int tentativesRestantes;
     private boolean estGagne;
-   
+
     public CadenasJeu() {
-        démarrerJeu();
+        demarrerJeu();
     }
-   
-    public final void démarrerJeu() {
-        codeSecret = générerCodeSecret();
+
+    public final void demarrerJeu() {
+        codeSecret = new int[LONGUEUR_CODE];
+        for (int i = 0; i < LONGUEUR_CODE; i++) {
+            codeSecret[i] = RAND.nextInt(10);
+        }
         tentativesRestantes = MAX_TENTATIVES;
         estGagne = false;
     }
-   
-    private int[] générerCodeSecret() {
-        Random rand = new Random();
-        int[] code = new int[LONGUEUR_CODE];
-        for (int i = 0; i < LONGUEUR_CODE; i++) {
-            code[i] = rand.nextInt(MAX_CHIFFRE - MIN_CHIFFRE + 1) + MIN_CHIFFRE;
-        }
-        return code;
-    }
-   
-    public int[] testerCombinaison(int[] essai) throws IllegalArgumentException {
+
+    public int[] testerCombinaison(int[] essai) {
         if (essai == null || essai.length != LONGUEUR_CODE) {
-            throw new IllegalArgumentException("L'essai doit être un tableau de " + LONGUEUR_CODE + " chiffres.");
+            throw new IllegalArgumentException("L'essai doit contenir " + LONGUEUR_CODE + " chiffres.");
         }
-       
-        if (estPartieTerminee() && !estGagne) {
+
+        if (estPartieTerminee()) {
             return new int[]{0, 0, 0};
         }
 
@@ -52,30 +48,28 @@ public class CadenasJeu {
         int tropBas = 0;
 
         for (int i = 0; i < LONGUEUR_CODE; i++) {
-            if (essai[i] == codeSecret[i]) {
-                exacts++;
-            } else if (essai[i] > codeSecret[i]) {
-                tropHauts++;
-            } else { // essai[i] < codeSecret[i]
-                tropBas++;
-            }
+            if (essai[i] == codeSecret[i]) exacts++;
+            else if (essai[i] > codeSecret[i]) tropHauts++;
+            else tropBas++;
         }
+
         tentativesRestantes--;
+
         if (exacts == LONGUEUR_CODE) {
             estGagne = true;
         }
-       
+
         return new int[]{exacts, tropHauts, tropBas};
     }
-   
+
     public boolean estPartieTerminee() {
         return estGagne || tentativesRestantes <= 0;
     }
-   
+
     public int getTentativesEffectuees() {
         return MAX_TENTATIVES - tentativesRestantes;
     }
-   
+
     public int getMaxTentatives() {
         return MAX_TENTATIVES;
     }
@@ -83,17 +77,10 @@ public class CadenasJeu {
     public boolean estGagne() {
         return estGagne;
     }
-   
+
     public String getCodeSecretString() {
-         return arrayToString(codeSecret);
-    }
-   
-    private String arrayToString(int[] arr) {
-        if (arr == null) return "";
         StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            sb.append(i);
-        }
+        for (int i : codeSecret) sb.append(i);
         return sb.toString();
     }
 }
